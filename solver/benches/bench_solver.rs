7cc76f8a69c5e0c8a7387c5df17bd60862fa8541
Pepijn Dragt <pepijn.dragt@gmail.com>
2021-06-19 23:29:55 +0200

    killer benchmark

--- solver/benches/bench_solver.rs
+++ solver/benches/bench_solver.rs
@@ -1,6 +1,6 @@
 #![feature(duration_constants)]
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use solver::{Sudoku, solvers::CageSolver};
+use solver::{rules::Rules, solvers::CageSolver, Config, Sudoku};
 
 static INPUT: &[(&str, &str)] = &[
     (
@@ -30,8 +30,8 @@ fn solver_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-fn sums_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Sums");
+fn killer_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Killer");
 
     group.bench_function("sums", |b| {
         b.iter(|| {
@@ -43,12 +43,42 @@ fn sums_benchmark(c: &mut Criterion) {
         })
     });
 
+    let sudoku = Sudoku::from(
+        "8.1...9....927.....5....4.3.............5.....3............3......8...4..8.5.4...",
+    );
+
+    let cages = solver::rules::Cages {
+        cages: vec![
+            15, 7, 15, 25, 5, 13, 6, 12, 4, 11, 9, 8, 5, 12, 23, 33, 16, 12, 9, 1, 5, 12, 14, 22,
+            13, 18, 9, 8, 8, 25, 8, 6, 7, 9,
+        ],
+        cells: [
+            1, 1, 2, 2, 3, 3, 4, 4, 4, 5, 6, 6, 2, 3, 7, 4, 8, 9, 5, 10, 10, 11, 12, 13, 13, 8, 9,
+            14, 15, 15, 16, 16, 16, 16, 17, 17, 14, 15, 18, 18, 18, 16, 19, 19, 17, 20, 21, 21, 22,
+            22, 16, 19, 23, 24, 25, 26, 26, 22, 27, 28, 28, 23, 24, 25, 26, 29, 30, 30, 30, 31, 31,
+            24, 32, 26, 29, 33, 33, 30, 31, 34, 24,
+        ],
+    };
+    let rules = Rules { cages };
+    let mut config = Config {
+        rules,
+        ..Default::default()
+    };
+    config.add_rules_solvers();
+    
+    group.bench_with_input("sudoku", &config, |b, c| {
+        b.iter(|| {
+            let config = c.clone();
+            black_box(sudoku.solve(Some(config)));
+        })
+    });
+
     group.finish();
 }
 
-criterion_group!{
+criterion_group! {
     name = benches;
     config = Criterion::default();
-    targets = solver_benchmark, sums_benchmark
+    targets = solver_benchmark, killer_benchmark
 }
 criterion_main!(benches);
